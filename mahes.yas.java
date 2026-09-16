ad36296ecab0a3186b39a19f5e1cb2ef23f94ac9
waranmagesh551-design <waranmagesh551@gmail.com>
2026-09-16 16:11:45 +0530

    Add files via upload

---
+++ mahes.yas.java
@@ -0,0 +1,16 @@
+public class WrapperClassDemo
+{
+        public static void main(String[] args)
+        {
+                    int primitiveInt = 100;
+                    float primitiveFloat = 25.75f;
+                    char primitiveChar = 'A';
+                    boolean primitiveBoolean = true;
+                    Integer WrappedInt = primitiveInt;
+                    Float WrapperFloat = primitiveFloat;
+                    Character WrappedChar = primitiveFloat;
+                    Boolean WrappedBoolean = primitiveBoolean;
+                    System.out.println("Autoboxed Integer:"+WrappedInt); 
+                    System.out.println("Autoboxed Integer:"+WrappedInt);
+                    System.out.println("Autoboxed Integer:"+WrappedInt);
+                    System.out.println("Autoboxed Integer:"+WrappedInt);
